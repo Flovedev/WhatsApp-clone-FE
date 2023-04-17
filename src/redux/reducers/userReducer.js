fe864609d29ef51c75f195e8cc73de4562e496d0
Flovedev <flove.dev@gmail.com>
2023-04-17 11:27:55 +0100

    FE installation done

---
+++ src/redux/reducers/userReducer.js
@@ -0,0 +1,19 @@
+import { GET_USERS } from "../actions";
+
+const initialState = {
+  userList: [],
+};
+
+const userReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case GET_USERS:
+      return {
+        ...state,
+        userList: action.payload,
+      };
+    default:
+      return state;
+  }
+};
+
+export default userReducer;
