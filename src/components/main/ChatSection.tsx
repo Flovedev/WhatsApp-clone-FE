d591edf47c5dd015eb943e9eb8b846369cb520c3
Flovedev <flove.dev@gmail.com>
2023-04-19 13:17:46 +0100

    updates pull

--- src/components/main/ChatSection.tsx
+++ src/components/main/ChatSection.tsx
@@ -20,38 +20,18 @@ const ChatSection = () => {
     //   socket.on("welcome", (welcomeMessage) => {
     //     console.log(welcomeMessage);
     //   });
-
-<<<<<<< Updated upstream
-      socket.on("loggedIn", (onlineUsersList) => {
-        console.log(onlineUsersList);
-        setOnlineUsers(onlineUsersList);
-        setLoggedIn(true);
-      });
-
-      socket.on("updateOnlineUsersList", (updatedList) => {
-        setOnlineUsers(updatedList);
-      });
-
-      socket.on("newMessage", (newMessage) => {
-        console.log(newMessage);
-        setChatHistory((chatHistory) => [...chatHistory, newMessage.message]);
-      });
-    });
-=======
     //   // socket.emit("joinRoom", "room"); // UNIQUE ROOM NAME FOR EACH USER
-
     //   socket.on("newMessage", newMessage => {
     //     setChatHistory((allMessages) => [...allMessages, newMessage.message])
     //   })
->>>>>>> Stashed changes
   }, []);
-
 
   // const sendMessage = () => {
   //   const newMessage = {
   //     sender: "username",
   //     text: message,
   //     createdAt: new Date().toLocaleString("en-US"),
+  //chatId: "643eb5d96f5a618a067379d7",
   //   };
   //   socket.emit("sendMessage", { message: newMessage });
   //   setChatHistory([...chatHistory, newMessage]);
@@ -79,14 +59,18 @@ const ChatSection = () => {
         ))}
       </ListGroup>
       <div className="messages-section flex-grow-1 d-flex flex-column-reverse ">
-        {chatHistory.slice().reverse().map((message, index) => (
-          <SingleMessage data={message} key={index} />
-        ))}
+        {chatHistory
+          .slice()
+          .reverse()
+          .map((message, index) => (
+            <SingleMessage data={message} key={index} />
+          ))}
       </div>
       <div className="d-flex align-items-center py-2 px-2 top-bars">
         <SlEmotsmile className="top-icons mx-2" />
         <RiAttachment2 className="top-icons mx-2" />
-        <Form className="message-container"
+        <Form
+          className="message-container"
           onSubmit={(e) => {
             e.preventDefault();
             // sendMessage();
