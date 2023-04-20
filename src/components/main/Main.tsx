106e6f14d6f96246dfcf9cda589380d1e94f88f5
Eleni-Ke <97251568+Eleni-Ke@users.noreply.github.com>
2023-04-20 20:14:54 +0200

    OAuth works with cookies

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -4,15 +4,41 @@ import { Container, Row } from "react-bootstrap";
 import UsersMenu from "./UsersMenu";
 import { useState, useEffect } from "react";
 import ProfileMenu from "./ProfileMenu";
-import { getUsers } from "../../redux/actions";
+import { getUsers, setCurrentUser } from "../../redux/actions";
 import { useAppDispatch } from "../../redux/hooks";
+import Cookies from "js-cookie";
 
 const Main = () => {
   const dispatch = useAppDispatch();
   const [showUsersMenu, setShowUsersMenu] = useState(false);
   const [showProfileMenu, setShowProfileMenu] = useState(false);
 
+  const getMeInfo = async () => {
+    try {
+      const accessToken = localStorage.getItem("accessToken");
+      let res = await fetch(`${process.env.REACT_APP_BE_URL}/users/me`, {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+          "Content-Type": "application/json",
+        },
+      });
+      if (res.ok) {
+        let userInfoFromGoogle = await res.json();
+        console.log(userInfoFromGoogle);
+        dispatch(setCurrentUser(userInfoFromGoogle));
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   useEffect(() => {
+    const tokenCookie = Cookies.get("accessToken");
+    if (tokenCookie) {
+      localStorage.setItem("accessToken", tokenCookie);
+      getMeInfo();
+    }
     dispatch(getUsers());
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
