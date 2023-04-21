ab68e1aab6d25159d084fb5a5ef1d338f23503d7
Eleni-Ke <97251568+Eleni-Ke@users.noreply.github.com>
2023-04-21 11:32:28 +0200

    css registration/main fixed conflicts

--- src/components/registration/Registration.tsx
+++ src/components/registration/Registration.tsx
@@ -61,76 +61,7 @@ const Registration = () => {
               d="M32.4 6.4C29 2.9 24.3 1 19.5 1 9.3 1 1.1 9.3 1.2 19.4c0 3.2.9 6.3 2.4 9.1L1 38l9.7-2.5c2.7 1.5 5.7 2.2 8.7 2.2 10.1 0 18.3-8.3 18.3-18.4 0-4.9-1.9-9.5-5.3-12.9zM19.5 34.6c-2.7 0-5.4-.7-7.7-2.1l-.6-.3-5.8 1.5L6.9 28l-.4-.6c-4.4-7.1-2.3-16.5 4.9-20.9s16.5-2.3 20.9 4.9 2.3 16.5-4.9 20.9c-2.3 1.5-5.1 2.3-7.9 2.3zm8.8-11.1l-1.1-.5s-1.6-.7-2.6-1.2c-.1 0-.2-.1-.3-.1-.3 0-.5.1-.7.2 0 0-.1.1-1.5 1.7-.1.2-.3.3-.5.3h-.1c-.1 0-.3-.1-.4-.2l-.5-.2c-1.1-.5-2.1-1.1-2.9-1.9-.2-.2-.5-.4-.7-.6-.7-.7-1.4-1.5-1.9-2.4l-.1-.2c-.1-.1-.1-.2-.2-.4 0-.2 0-.4.1-.5 0 0 .4-.5.7-.8.2-.2.3-.5.5-.7.2-.3.3-.7.2-1-.1-.5-1.3-3.2-1.6-3.8-.2-.3-.4-.4-.7-.5h-1.1c-.2 0-.4.1-.6.1l-.1.1c-.2.1-.4.3-.6.4-.2.2-.3.4-.5.6-.7.9-1.1 2-1.1 3.1 0 .8.2 1.6.5 2.3l.1.3c.9 1.9 2.1 3.6 3.7 5.1l.4.4c.3.3.6.5.8.8 2.1 1.8 4.5 3.1 7.2 3.8.3.1.7.1 1 .2h1c.5 0 1.1-.2 1.5-.4.3-.2.5-.2.7-.4l.2-.2c.2-.2.4-.3.6-.5s.4-.4.5-.6c.2-.4.3-.9.4-1.4v-.7s-.1-.1-.3-.2z"
             ></path>
           </svg>
-<<<<<<< Updated upstream
-          Whatsapp Web
-        </span>
-      </div>
-      <div className="main-div">
-        <div className="login-info">
-          <h2>Register to WhatsApp</h2>
-          <Form
-            onSubmit={(e) => {
-              e.preventDefault();
-              userRegistration();
-            }}
-          >
-            <Form.Group controlId="formBasicUsername">
-              <Form.Label>Username</Form.Label>
-              <Form.Control
-                type="username"
-                placeholder="Set your username"
-                value={userName}
-                onChange={(e) => setUserName(e.target.value)}
-              />
-            </Form.Group>
-
-            <Form.Group controlId="formBasicInfo">
-              <Form.Label>Info</Form.Label>
-              <Form.Control
-                type="userInfo"
-                placeholder="Set your info"
-                value={userInfo}
-                onChange={(e) => setUserInfo(e.target.value)}
-              />
-            </Form.Group>
-
-            <Form.Group controlId="formBasicEmail">
-              <Form.Label>Email address</Form.Label>
-              <Form.Control
-                type="email"
-                placeholder="Enter email"
-                value={userEmail}
-                onChange={(e) => setUserEmail(e.target.value)}
-              />
-            </Form.Group>
-
-            <Form.Group controlId="formBasicPassword">
-              <Form.Label>Password</Form.Label>
-              <Form.Control
-                type="password"
-                placeholder="Set your password"
-                value={userPW}
-                onChange={(e) => setUserPW(e.target.value)}
-              />
-            </Form.Group>
-
-            <div className="btn-wrapper">
-              <Button className="reg-btn" onClick={userRegistration}>
-                Register to WhatsApp
-              </Button>
-              <a href={`${process.env.REACT_APP_BE_URL}/users/googleLogin`}>
-                <GoogleButton
-                  className="sign-in-w-google"
-                  type="dark"
-                  disabled={false}
-                  // onClick={() => {disabled={false}}}
-                ></GoogleButton>
-              </a>
-            </div>
-          </Form>
-=======
           WHATSAPP WEB
->>>>>>> Stashed changes
         </div>
 
         <div className="main-div">
